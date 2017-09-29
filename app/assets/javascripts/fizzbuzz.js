6daa60a6896e343780c0c93875bbf8299c02c5ae
David Ihnen <dihnen@pillartechnology.com>
2017-09-29 16:24:36 -0400

    event async

--- app/assets/javascripts/fizzbuzz.js
+++ app/assets/javascripts/fizzbuzz.js
@@ -1,27 +1,45 @@
-var fizz = (num) => {
-  if (isFizzy(num)) {
-    return "fizz"
+exports.execute = (num, cb) => {
+
+  const EventEmitter = require('events')
+  class emitter extends EventEmitter {}
+  em = new emitter()
+
+  var fizz = (num) => {
+    if (isFizzy(num) && !isBuzzy(num)) em.emit('out', "fizz")
   }
-  return num
-}
 
-var buzz = (num) => {
-  if (isBuzzy(num)) {
-    return "buzz"
+  var buzz = (num) => {
+    if (isBuzzy(num) && !isFizzy(num)) em.emit('out', "buzz")
   }
-  return num
-}
 
-var isFizzy = (num) => {
-  return num % 3 === 0
-}
+  var both = (num) => {
+    if (isBuzzy(num) && isFizzy(num)) em.emit('out', "fizzbuzz")
+  }
 
-var isBuzzy = (num) => {
-  return num % 5 === 0
-}
+  var neither = (numb) => {
+    if (flat(numb)) em.emit('out', numb + '')
+  }
 
-exports.execute = (num) => {
-  if (isFizzy(num) && isBuzzy(num))
-    return "fizzbuzz"
-  return buzz(fizz(num)) + ""
+  em.on('number', fizz)
+  em.on('number', buzz)
+  em.on('number', both)
+  em.on('number', neither)
+
+  var flat = (num) => {
+    return !isFizzy(num) && !isBuzzy(num)
+  }
+
+  var isFizzy = (num) => {
+    return num % 3 === 0
+  }
+
+  var isBuzzy = (num) => {
+    return num % 5 === 0
+  }
+
+  counter = 0
+  buffer = []
+
+  em.on('out', cb)
+  em.emit('number', num)
 }
--- spec/javascripts/fizzbuzz_spec.js
+++ spec/javascripts/fizzbuzz_spec.js
@@ -2,48 +2,54 @@ var underTest = require('../..')
 
 describe('fizzbuzz', function() {
   describe(' ', function() {
-    it('should return string 1 when fizzbuzz of 1',
-      function(done) {
-        let result = underTest.execute(1)
+    it('should return string 1 when fizzbuzz of 1', (done) => {
+      let result = underTest.execute(1, (result) => {
         expect(result).toBe("1")
         done()
       })
+    })
     it('should return string 2 when fizzbuzz of 2',
       function(done) {
-        let result = underTest.execute(2)
-        expect(result).toBe("2")
-        done()
+        let result = underTest.execute(2, (result) => {
+          expect(result).toBe("2")
+          done()
+        })
       })
     it('should return string fizz when fizzbuzz of 3',
       function(done) {
-        let result = underTest.execute(3)
-        expect(result).toBe("fizz")
-        done()
+        underTest.execute(3, (result) => {
+          expect(result).toBe("fizz")
+          done()
+        })
       })
     it('should return string buzz when fizzbuzz of 5',
       function(done) {
-        let result = underTest.execute(5)
-        expect(result).toBe("buzz")
-        done()
+        underTest.execute(5, (result) => {
+          expect(result).toBe("buzz")
+          done()
+        })
       })
     it('should return string fizz when fizzbuzz of 6',
       function(done) {
-        let result = underTest.execute(6)
-        expect(result).toBe("fizz")
-        done()
+        underTest.execute(6, (result) => {
+          expect(result).toBe("fizz")
+          done()
+        })
       })
 
     it('should return string buzz when fizzbuzz of 10',
       function(done) {
-        let result = underTest.execute(10)
-        expect(result).toBe("buzz")
-        done()
+        underTest.execute(10, (result) => {
+          expect(result).toBe("buzz")
+          done()
+        })
       })
     it('should return string fizzbuzz when fizzbuzz of 15',
       function(done) {
-        let result = underTest.execute(15)
-        expect(result).toBe("fizzbuzz")
-        done()
+        underTest.execute(15, (result) => {
+          expect(result).toBe("fizzbuzz")
+          done()
+        })
       })
   })
 })
